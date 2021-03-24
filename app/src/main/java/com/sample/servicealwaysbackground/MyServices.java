2017f77ef24ed84debbd5a60cd706ee7e37236f6
acastor <anthony@globalmobility.net>
2021-03-25 02:20:48 +0800

    first commit

---
+++ app/src/main/java/com/sample/servicealwaysbackground/MyServices.java
@@ -0,0 +1,31 @@
+package com.sample.servicealwaysbackground;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+public class MyServices extends Service {
+    public MyServices() {
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        onTaskRemoved(intent);
+        Toast.makeText(getApplicationContext(),"This is a Service running in Background",
+                Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Intent restartServiceIntent = new Intent(getApplicationContext(),this.getClass());
+        Log.i("MyActivity", "Our Service on the background ");
+        restartServiceIntent.setPackage(getPackageName());
+        startService(restartServiceIntent);
+        super.onTaskRemoved(rootIntent);
+    }
+}
